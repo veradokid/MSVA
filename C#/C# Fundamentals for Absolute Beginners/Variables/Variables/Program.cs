0f43ea3e476ee718e31e0b350391a1f8c4b14b47
Johannes Bø <johannes.bo@gmail.com>
2016-08-29 12:35:57 +0200

    x+y

--- C#/C# Fundamentals for Absolute Beginners/Variables/Variables/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/Variables/Variables/Program.cs
@@ -10,6 +10,14 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
         }
     }
 }
