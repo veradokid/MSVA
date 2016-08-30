ed0d3101bfc07bcbd6ad0ea6626621525a768e01
Johannes Bø <johannes.bo@gmail.com>
2016-08-30 11:13:04 +0200

    cleanup, declaring var only once

--- C#/C# Fundamentals for Absolute Beginners/Decisions/Decisions/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/Decisions/Decisions/Program.cs
@@ -14,27 +14,25 @@ namespace Decisions
             Console.Write("Choose a door: 1,2 or 3: ");
             string userValue = Console.ReadLine();
 
+            string message = "";
+
             if (userValue == "1")
             {
-                string message = "You won a new car!";
-                Console.WriteLine(message);
+                message = "You won a new car!";
             }
             else if (userValue == "2")
             {
-                string message = "You won a new boat!";
-                Console.WriteLine(message);
+                message = "You won a new boat!";
             }
             else if (userValue == "3")
             {
-                string message = "You won a new cat!";
-                Console.WriteLine(message);
+                message = "You won a new cat!";
             }
             else
             {
-                string message = "Sorry, we didn't understand.";
-                Console.WriteLine(message);
+                message = "Sorry, we didn't understand.";
             }
-
+            Console.WriteLine(message);
             Console.ReadLine();
 
         }
