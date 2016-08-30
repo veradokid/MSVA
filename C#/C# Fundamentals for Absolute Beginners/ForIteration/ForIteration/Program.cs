f937ec5019abd73e6765286a860abd8709e252bd
Johannes Bø <johannes.bo@gmail.com>
2016-08-30 11:51:43 +0200

    Added For i = 0..9 iteration

--- C#/C# Fundamentals for Absolute Beginners/ForIteration/ForIteration/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/ForIteration/ForIteration/Program.cs
@@ -10,6 +10,11 @@ namespace ForIteration
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }
