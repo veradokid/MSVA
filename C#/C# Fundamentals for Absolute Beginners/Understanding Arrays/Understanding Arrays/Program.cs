899db305cc0dcb1ad5ac760803ec4e343fca21aa
Johannes Bø <johannes.bo@gmail.com>
2016-08-30 12:24:43 +0200

    ex 1 finnished

--- C#/C# Fundamentals for Absolute Beginners/Understanding Arrays/Understanding Arrays/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/Understanding Arrays/Understanding Arrays/Program.cs
@@ -10,6 +10,27 @@ namespace Understanding_Arrays
     {
         static void Main(string[] args)
         {
+            /* ex 1
+            int[] numbers = new int[5];     //declare the array
+
+            //set values
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+            //numbers[5] = 42;    //will not run.
+
+
+            //access values inside elements
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers.Length);      //.length
+
+            Console.ReadLine();
+            */
+
+            //ex 2
+
         }
     }
 }
