b38be17d36b8b079ef94a18302d1a203f5c062f9
Johannes Bø <johannes.bo@gmail.com>
2016-08-30 11:44:16 +0200

    many examples -more like documentation NOT a program

--- C#/C# Fundamentals for Absolute Beginners/OperatorsExpressionsStatements/OperatorsExpressionsStatements/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/OperatorsExpressionsStatements/OperatorsExpressionsStatements/Program.cs
@@ -10,6 +10,68 @@ namespace OperatorsExpressionsStatements
     {
         static void Main(string[] args)
         {
+            // Variable declareation
+            int x, y, a, b;
+
+            // Assign operator
+            x = 3;
+            y = 2;
+            a = 1;
+            b = 0;
+
+            // There are many mathematical operators...
+
+            // Addition operator
+            x = 3 + 4;
+
+            //Subtraction operator
+            x = 4 - 3;
+
+            //Multiplication operator
+            x = 10 * 5;
+
+            //Division operator
+            x = 10 / 5;
+
+            //Order of operations usin paranthesis
+            x = (x + y) * (a - b);
+
+            // There are many operators used to evaluate values...
+
+            // Equality operator
+            if (x == y)
+            { }
+            // Equality operator
+            if (x > y)
+            { }
+            // Equality operator
+            if (x < y)
+            { }
+            // Equality operator
+            if (x >= y)
+            { }
+            // Equality operator
+            if (x <= y)
+            { }
+
+            // There are two "contitional" operators as well that can
+            // be used to expand / enhance an evaluation ...
+            // ... and they can be combined together multiple times.
+
+            // Conditional AND operator _
+            if ((x > y) && (a > b)) { }
+
+            // Conditional OR operator _
+            if ((x > y) || (a > b)) { }
+
+            // Also, here's the inline contditional operator we
+            // learned about in the previous lesson ...
+            string message = (x == 1) ? "Car" : "Boat";
+
+            // Member access and Method invocation
+            Console.WriteLine("Hi");    // [.] Member access. [()] Invoke/ execute the member method
+
+
         }
     }
 }
