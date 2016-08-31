4fe902e2a27839801a8e7e853a3f2982e00ccd65
Johannes Bø <johannes.bo@gmail.com>
2016-08-31 10:19:54 +0200

    cleanup. session 11 finnished

--- C#/C# Fundamentals for Absolute Beginners/HelperMethods/HelperMethods/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/HelperMethods/HelperMethods/Program.cs
@@ -21,42 +21,6 @@ namespace HelperMethods
             Console.Write("In what city were you born?");
             string city = Console.ReadLine();
 
-            //char[] firstNameArray = firstName.ToCharArray();
-            //Array.Reverse(firstNameArray);
-
-            //char[] lastNameArray = lastName.ToCharArray();
-            //Array.Reverse(lastNameArray);
-
-            //char[] cityArray = city.ToCharArray();
-            //Array.Reverse(cityArray);
-
-            //string result = "";
-
-            //foreach (char item in firstNameArray)
-            //{
-            //    result += item;
-            //}
-
-            //result += " ";
-
-            //foreach (char item in lastNameArray)
-            //{
-            //    result += item;
-            //}
-
-            //result += " ";
-
-            //foreach (char item in cityArray)
-            //{
-            //    result += item;
-            //}
-
-            //Console.WriteLine("Results: " + result);
-
-            //string reversedFirstName = ReverseString(firstName);
-            //string reversedLastName = ReverseString(lastName);
-            //string reversedCity = ReverseString(city);
-
             DisplayResult(
                 ReverseString(firstName),
                 ReverseString(lastName),
@@ -97,15 +61,6 @@ namespace HelperMethods
             Console.Write("Result: ");
             Console.Write(message);
         }
-
-
-        //foreach (char item in messageArray)
-        //{
-        //    Console.Write(item);
-        //}
-        //Console.Write(" ");
-
-
 
     }//program
 }
