2cd6f380ebe445908b4954c336716defaf1ca460
Johannes Bø <johannes.bo@gmail.com>
2016-08-31 10:37:27 +0200

    working menu

--- C#/C# Fundamentals for Absolute Beginners/WhileIteration/WhileIteration/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/WhileIteration/WhileIteration/Program.cs
@@ -10,6 +10,51 @@ namespace WhileIteration
     {
         static void Main(string[] args)
         {
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
         }
+
+        //developer methods here
+        private static bool MainMenu()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Option 1");
+            Console.WriteLine("2. Option 2");
+            Console.WriteLine("3. Exit");
+            string result = Console.ReadLine();
+            if (result == "1")
+            {
+                PrintNumbers();
+                return true;
+            }
+            else if (result == "2")
+            {
+                GuessingGame();
+                return true;
+            }
+            else if (result == "3")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }//MainMenu
+
+        private static void PrintNumbers()
+        {
+            Console.WriteLine("Print numbers");
+            Console.ReadLine();
+        }//PrintNumbers
+
+        private static void GuessingGame()
+        {
+            Console.WriteLine("Guessing game");
+            Console.ReadLine();
+        }//GuessingGame
     }
 }
