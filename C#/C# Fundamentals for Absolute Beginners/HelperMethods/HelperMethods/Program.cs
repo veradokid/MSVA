1f16cbf74a9be74d979b710a0571ddb3dbeeb295
Johannes Bø <johannes.bo@gmail.com>
2016-08-31 09:52:25 +0200

    Method with return value

--- C#/C# Fundamentals for Absolute Beginners/HelperMethods/HelperMethods/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/HelperMethods/HelperMethods/Program.cs
@@ -54,26 +54,31 @@ namespace HelperMethods
             //Console.WriteLine("Results: " + result);
 
             Console.Write("Result: ");
-            ReverseString(firstName);
-            ReverseString(lastName);
-            ReverseString(city);
+            string reversedFirstName = ReverseString(firstName);
+            string reversedLastName = ReverseString(lastName);
+            string reversedCity = ReverseString(city);
+
+            Console.Write(String.Format("{0} {1} {2}", 
+                reversedFirstName,
+                reversedLastName,
+                reversedCity));
 
             Console.ReadLine();
 
         }//main
 
-        private static void ReverseString(string message)
+        private static string ReverseString(string message)
         {           
             char[] messageArray = message.ToCharArray();
             Array.Reverse(messageArray);
-
-            foreach (char item in messageArray)
-            {
-                Console.Write(item);
-            }
-            Console.Write(" ");
+            return String.Concat(messageArray);
         }
 
+    //foreach (char item in messageArray)
+    //{
+    //    Console.Write(item);
+    //}
+    //Console.Write(" ");
 
 
 
