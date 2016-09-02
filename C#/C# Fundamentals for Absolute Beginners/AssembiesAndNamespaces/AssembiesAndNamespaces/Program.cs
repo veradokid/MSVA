be0e3b4d1a417c38f4c8ceb11ede17ab9b2e12f8
Johannes Bø <johannes.bo@gmail.com>
2016-09-02 20:55:52 +0200

    som comments about namespaces

--- C#/C# Fundamentals for Absolute Beginners/AssembiesAndNamespaces/AssembiesAndNamespaces/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/AssembiesAndNamespaces/AssembiesAndNamespaces/Program.cs
@@ -10,6 +10,8 @@ namespace AssembiesAndNamespaces
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
