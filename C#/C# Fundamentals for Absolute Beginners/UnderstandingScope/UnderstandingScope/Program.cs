eb6e87c2a1663a096b773528c0073fbd9071e039
Johannes Bø <johannes.bo@gmail.com>
2016-09-02 19:29:37 +0200

    in or out of scope. comments and some code examples

--- C#/C# Fundamentals for Absolute Beginners/UnderstandingScope/UnderstandingScope/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/UnderstandingScope/UnderstandingScope/Program.cs
@@ -8,8 +8,37 @@ namespace UnderstandingScope
 {
     class Program
     {
+        private static string k = "";
+
         static void Main(string[] args)
         {
+
+            string j = "";  //j is defined outside the for loop. available both places.
+
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(i);
+
+                if (i == 9)
+                {
+                    string l = i.ToString(i);
+                }
+                //Console.WriteLine(l);   //not in scope
+            }
+            //Console.WriteLine(i);   //will not compile: i out of scope
+
+            Console.WriteLine("Outside of the scope, j = " + j);
+            Console.WriteLine("Outside of the for-loop, k = " + k);
+            HelperMethod();
+            Console.ReadLine();
         }
+
+        static void HelperMethod()
+        {
+            Console.WriteLine("Value k from the HelperMethod():" + k);
+        }
+        
     }
 }
