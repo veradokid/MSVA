6d2354b9884a7bc9817d57d1a3e5275f25a32f9f
Johannes Bø <johannes.bo@gmail.com>
2016-09-02 21:20:03 +0200

    writing to file after ex on web

--- C#/C# Fundamentals for Absolute Beginners/AssembiesAndNamespaces/AssembiesAndNamespaces/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/AssembiesAndNamespaces/AssembiesAndNamespaces/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AssembiesAndNamespaces
 {
@@ -10,8 +11,14 @@ namespace AssembiesAndNamespaces
     {
         static void Main(string[] args)
         {
+            string text = "A class is the most powerful data type in C#. Like a structure, " +
+                           "a class defines the data and behavior of the data type. ";
+
+            //System.IO.File.WriteAllText(@"C:\Lesson17\\WriteText.txt", text);
+            File.WriteAllText(@"C:\MSVA\WriteText.txt", text); //included nameespace in using
+
             Console.WriteLine("Hello World");
             Console.ReadLine();
-        }
-    }
+        }//Main
+    }//Program
 }
