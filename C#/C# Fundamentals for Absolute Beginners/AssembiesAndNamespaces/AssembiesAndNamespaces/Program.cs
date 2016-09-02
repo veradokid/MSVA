2e425068a0ae4728318200078e81e53d95c6d868
Johannes Bø <johannes.bo@gmail.com>
2016-09-02 21:29:31 +0200

    reading a html file as text

--- C#/C# Fundamentals for Absolute Beginners/AssembiesAndNamespaces/AssembiesAndNamespaces/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/AssembiesAndNamespaces/AssembiesAndNamespaces/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Net;
 
 namespace AssembiesAndNamespaces
 {
@@ -11,11 +12,22 @@ namespace AssembiesAndNamespaces
     {
         static void Main(string[] args)
         {
+            //source snippet #1: https://msdn.microsoft.com/en-us/library/8bh11f1k.aspx
             string text = "A class is the most powerful data type in C#. Like a structure, " +
                            "a class defines the data and behavior of the data type. ";
 
             //System.IO.File.WriteAllText(@"C:\Lesson17\\WriteText.txt", text);
             File.WriteAllText(@"C:\MSVA\WriteText.txt", text); //included nameespace in using
+                                                               // end snippet #1
+
+
+            //source snippet #2: https://msdn.microsoft.com/en-us/library/fhd1f0sw(v=vs.110).aspx
+            //WebClient is not initially recognized
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+            //end snippet #2
 
             Console.WriteLine("Hello World");
             Console.ReadLine();
