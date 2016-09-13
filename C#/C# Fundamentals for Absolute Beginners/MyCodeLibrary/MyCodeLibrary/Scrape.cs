6ec09af9130d7bd9bff0ab16294f5db2c8e2b8e5
Johannes Bø <johannes.bo@gmail.com>
2016-09-13 10:19:20 +0200

    Created a class:Scrape inside a separate file: a library

--- C#/C# Fundamentals for Absolute Beginners/MyCodeLibrary/MyCodeLibrary/Scrape.cs
+++ C#/C# Fundamentals for Absolute Beginners/MyCodeLibrary/MyCodeLibrary/Scrape.cs
@@ -12,22 +12,26 @@ namespace MyCodeLibrary
     {
         public string ScrapeWebpage(string url)
         {
-            WebClient client = new WebClient();
-            return client.DownloadString(url);
+            //WebClient client = new WebClient();
+            //return client.DownloadString(url);
+            return GetWebPage(url);
         }
 
         public string ScrapeWebpage(string url, string filepath)
         {
-            WebClient client = new WebClient();
-            string reply = client.DownloadString(url);
+            //WebClient client = new WebClient();
+            //string reply = client.DownloadString(url);
+            string reply = GetWebPage(url);
 
             File.WriteAllText(filepath, reply);
             return reply;
         }
 
         private string GetWebPage(string url)
+        //helpermethod to eliminate duplicate code
         {
-
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
         }
     }
 }
