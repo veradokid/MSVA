c99f0448ea7d0356c8586443beb15359badb331b
Johannes Bø <johannes.bo@gmail.com>
2016-09-13 11:06:28 +0200

    instatiate and populate wit ex data

--- C#/C# Fundamentals for Absolute Beginners/WorkingWithCollections/WorkingWithCollections/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/WorkingWithCollections/WorkingWithCollections/Program.cs
@@ -10,6 +10,19 @@ namespace WorkingWithCollections
     {
         static void Main(string[] args)
         {
+            //Instatiate and populate with values
+            Car car1 = new Car();
+            car1.Make = "Oldsmobile";
+            car1.Model "Cutlas Supreme";
+
+            Car car2 = new Car();
+            car2.Make = "Geo";
+            car2.Model = "Prism";
+
+            Book b1 = new Book();
+            b1.Author = "Robert Tabor";
+            b1.Title = "Microsoft .NET XML Web Services";
+            b1.ISBN = "0-000-00000-0";
         }
     }
 
