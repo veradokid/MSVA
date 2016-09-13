ab0bc9c0f2045dcd27f7fbb77751ba7a3d100f67
Johannes Bø <johannes.bo@gmail.com>
2016-09-13 10:51:29 +0200

    working example of solution including two sub-projects

---
+++ C#/C# Fundamentals for Absolute Beginners/Lesson 19/ScrapeLibrary/Scrape.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebPage(url);
+        }
+
+        public string ScrapeWebpage(string url, string filepath)
+        {
+            string reply = GetWebPage(url);
+
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+
+        private string GetWebPage(string url)
+        //helpermethod to eliminate duplicate code
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+
+
+}
