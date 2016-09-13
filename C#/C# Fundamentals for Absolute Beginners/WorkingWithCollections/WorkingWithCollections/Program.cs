f6617e7bfb38df611ff8e9f424c81f1b1747ef1e
Johannes Bø <johannes.bo@gmail.com>
2016-09-13 11:03:20 +0200

    Added properties

--- C#/C# Fundamentals for Absolute Beginners/WorkingWithCollections/WorkingWithCollections/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/WorkingWithCollections/WorkingWithCollections/Program.cs
@@ -15,11 +15,14 @@ namespace WorkingWithCollections
 
     class Car
     {
-
+        public string Make { get; set; }
+        public string Model { get; set; }
     }
 
     class Book
     {
-
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
     }
 }
