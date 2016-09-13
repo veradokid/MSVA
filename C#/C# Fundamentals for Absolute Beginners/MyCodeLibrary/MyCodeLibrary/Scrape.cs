580f9ffaa1bbc249bd787a843fd5d65ee5fda9eb
Johannes Bø <johannes.bo@gmail.com>
2016-09-13 10:19:53 +0200

    code cleanup

--- C#/C# Fundamentals for Absolute Beginners/MyCodeLibrary/MyCodeLibrary/Scrape.cs
+++ C#/C# Fundamentals for Absolute Beginners/MyCodeLibrary/MyCodeLibrary/Scrape.cs
@@ -8,19 +8,15 @@ using System.Threading.Tasks;
 
 namespace MyCodeLibrary
 {
-    public class Ccrape
+    public class Scrape
     {
         public string ScrapeWebpage(string url)
         {
-            //WebClient client = new WebClient();
-            //return client.DownloadString(url);
             return GetWebPage(url);
         }
 
         public string ScrapeWebpage(string url, string filepath)
         {
-            //WebClient client = new WebClient();
-            //string reply = client.DownloadString(url);
             string reply = GetWebPage(url);
 
             File.WriteAllText(filepath, reply);
