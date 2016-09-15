fa911ee0383f80b91b75164de87332f309c5c4fa
Johannes Bø <johannes.bo@gmail.com>
2016-09-15 14:32:22 +0200

    lesson 22 : finnished

--- C#/C# Fundamentals for Absolute Beginners/HandlingExceptions/HandlingExceptions/Program.cs
+++ C#/C# Fundamentals for Absolute Beginners/HandlingExceptions/HandlingExceptions/Program.cs
@@ -16,11 +16,26 @@ namespace HandlingExceptions
                 string content = File.ReadAllText(@"c:\Lesson22\Exampl.txt");
                 Console.WriteLine(content);
             }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("FileNotFound!");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("DirectoryNotFound!");
+            }
             catch (Exception ex)
             {
                 //throw;
                 Console.WriteLine("There was a problem!");
                 Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Code to finalize
+                // Setting objects to null
+                // Closing database connection
+                Console.WriteLine("Closing the app now!");
             }
             Console.ReadLine();
         }
